ff2342a87da365cbc8176fb23d21d2b3e03d5f67
Blake Griffith <blake.a.griffith@gmail.com>
2012-11-29 16:22:47 -0600

    Did problem 6 and saved output

--- hw11/hw11.py
+++ hw11/hw11.py
@@ -16,9 +16,53 @@ class three:
     Tq  = (P * (h**3) /((2*math.pi*m)**(3./2.) * k**(5./2.)))**(2./5.)
 
 class five:
+    k   = 8.617e-5          # eV/K
     # Fermi-Dirac probability of a state being occupied.
-    def Pfd(self, e, u, b):
-        return (math.exp(-(e-u)/float(b)) + 1)**-1
+    def Pfd( dif, b=(1./(298*k))):
+        return (math.exp(dif/float(b)) + 1)**(-1.)
+    # (a)
+    a = Pfd(-1.)
+    # (b)
+    b = Pfd(-0.01)
+    # (c)
+    c = Pfd(0.)
+    # (d)
+    d = Pfd(0.01)
+    # (e)
+    e = Pfd(1.)
+
+class six:
+    k   = 8.617e-5          # eV/K
+    B   = 1./(k*298)
+    n   = [0, 1, 2, 3]
+    dE_list  = [0.001, 0.01, 0.1, 1.]
+
+    for dE in dE_list:
+
+        # Average occupancy.
+        avgn = (math.exp(dE*B) - 1)**-1.
+        print "Average ocuupancy at {} eV is {} particles".format(dE, avgn)
+        print " and:"
+
+        # Partition function.
+        Z = (1 - math.exp(-dE*B))**-1.
+        
+
+        # Probability for state to have n[j] particles.
+        for j in range(len(n)):
+            P_sum = 0
+            for k in range(j+1):
+                P_sum += math.exp(-dE*B)
+            P = (1./Z)*P_sum
+            print "\t Probability of state containing {} bosons is {}".format(j, P)
+
+
+
+
+
+
+
+
 
 
 
